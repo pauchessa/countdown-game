0328a473e325bab55cd1ecb28ffd732f37aefd98
pauchessa <lisitsinarenard18@gmail.com>
2026-05-22 17:34:23 +0400

    added name editing functionality

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState("");
+  const [edited, setEdited] = useState(false);
+
+  function handleChangeName(event) {
+    setEdited(false);
+    setPlayerName(event.target.value);
+  }
+
+  function handleClick() {
+    setEdited(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {edited ? playerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" value={playerName} onChange={handleChangeName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
